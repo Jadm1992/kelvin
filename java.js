74a05b3e271522b493e63d4242eb4f23f905c958
Devyon Miller <devyonmiller8@gmail.com>
2022-12-11 20:01:21 -0500

    codecademy kelvin weather project

---
+++ java.js
@@ -0,0 +1,178 @@
+// var userInp = +prompt('Enter a number')
+
+// var percentage = userInp * 0.20
+// document.write('Percentage ='+percentage)
+
+//write a program that will ask the user to enter a price,
+// the program will add a 20% tax on the price and show the user 
+// the total payable amount
+
+// var price = +prompt('Enter an amount')
+
+// var percentage = price * 0.20
+
+// var totalAmount = price + percentage
+
+// document.write(totalAmount+totalAmount)
+
+// var price = +prompt('Enter a number')
+
+// var percentage = price * 0.45
+
+// var answer  = price - percentage
+
+// document.write('Answer = '+answer)
+
+// write a program that will ask the user to enter his her salary if the salary is less then 1000 then add
+// a 20% increment int the user's saley. Otherwise display the original saley which
+
+// How to calculate the percentage of things
+// var salary = +prompt('Enter your salary')
+
+// if (salary < 1000) {
+//     var percentage  = salary * 0.20
+//     var newSalary = salary + percentage
+//     document.write('This is your salary =' +newSalary)
+// }
+
+// else {
+//     document.write('This is your salary =' +salary)
+// }
+
+// for (let i = 1; i <= **; i += 2) {
+//     document.write(i +"<br/>");
+//   };
+
+// var userInp = +prompt('Enter a number')
+// for (var i = 1; i <= userInp; i++) {
+//     document.write(i +"<br/>");
+//   };
+
+
+
+
+
+// window.addEventListener('load', () =>{
+//   const form = document.querySelector("#new-task-form")
+//   const input = document.querySelector("#new-task-input")
+//   const list_el = document.querySelector("#tasks")
+
+//   console.log(form)
+// })
+//////////////////////////////////// TENARY OPERATOR///////////////////////////////////////////////////////
+// condition ? 'True' : 'False'
+
+//////////////////////////////////// Destructuring///////////////////////////////////////////////////////
+// With Destructuring
+// const objects = ['table', 'iPhone', 'apple']
+// const [furniture, mobile, fruit] = objects
+
+// Without Destructuring
+// const furniture = objects[0]
+// const mobile = objects[1]
+// const fruit = objects[2]
+
+//////////////////////////////////// The Spread operator///////////////////////////////////////////////////////
+// const [person, setPerson] = useState({
+//   id: '',
+//   name: '',
+//   age: ''
+// });
+
+// setPerson([
+//           ...person,
+//           {
+//               id:"1",
+//               name: "Steve",
+//               age:"25"
+//           }
+//       ]);
+
+//////////////////////////////////// Array methods///////////////////////////////////////////////////////
+
+
+//////////////////////////////////// Arrow Functions///////////////////////////////////////////////////////
+// Regular Functions
+// function hello() {
+//   return 'hello'
+// }
+
+// // Arrow Functions
+// let hello = () => 'hello'
+
+//////////////////////////////////// Promises///////////////////////////////////////////////////////
+// let promise = new Promise((resolve, reject) => {
+//   const i = "Promise";
+//   i === "Promise" ? resolve() : reject(); // Checkout the above Ternary Operator section to better understand the syntax
+//   }
+// );
+
+// promise.
+//     then(() => {
+//         console.log('Your promise is resolved');
+//     }).
+//     catch(() => {
+//         console.log('Your promise is rejected');
+//     });
+
+//////////////////////////////////// The Fetch API///////////////////////////////////////////////////////
+// fetch('http://example.com/books.json') // fetching the resource URL
+//   .then(response => response.json()); // calling .json() method on the promise
+//   .then(data => setState(data)); // updating the state with the JSON data
+
+//////////////////////////////////// Async/Await///////////////////////////////////////////////////////
+// async function asyncFunction() {
+//   let promise = new Promise(resolve => {
+//       resolve();
+//   });
+//   let response = await promise; // further execution will be stopped until the promise is resolved or rejected
+//   return console.log(response);
+// }
+
+//////////////////////////////////// ES modules and Import/Export///////////////////////////////////////////////////////
+// function Component() {
+//   return(
+//       <div>This is a component</div>
+//   )
+// }
+
+// export default Component
+
+// async function asyncFunction() {
+//   let promise = new Promise(resolve => {
+//       resolve();
+//   });
+//   let response = await promise; // further execution will be stopped until the promise is resolved or rejected
+//   return console.log(response);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practice.js
@@ -0,0 +1,18 @@
+
+// I am assigning the variable kelvin to the expression 293
+const kelvin = 293;
+// here I use the let identifier and I'm assigning the variable celsius to the expression 273
+const celsius = 273;
+// next we console log the kelvin assign the subtraction assignment operator. Any value to the 
+// right of the operand from a variable and assigns the result to the variable. 
+console.log(kelvin - celsius)
+// here we assign the let identifier to the variable fahrenheit and give it the value 33.8
+// Fahrenheit = Celsius * (9/5) + 32
+let fahrenheit = 33.8
+// Math.round(33.8)
+// next we want to round the number saved to fahrenheit. So in order to do that we use the 
+// Math.round method from the built in Math object.
+// console.log(Math.round(fahrenheit))
+// here we use the .floor method to round down the fahrenheit temperature.
+console.log(Math.floor(fahrenheit))
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
